54fd9f72f05d9f4100b8d8e3c2dc3f44c930b9d2
Ashwath Kumaran <a4ashwath@gmail.com>
2026-01-19 23:31:08 +0530

    Included the docker ignore file and made few changes

--- pms-client/src/container/applayout/index.tsx
+++ pms-client/src/container/applayout/index.tsx
@@ -2,7 +2,13 @@ import type { JSX } from "react/jsx-dev-runtime";
 
 function AppLayout({screen} : {screen: JSX.Element}) {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh"
+      }}
+    >
         {screen}
     </div>
   );
--- pms-client/src/container/home/index.tsx
+++ pms-client/src/container/home/index.tsx
@@ -1,8 +1,10 @@
 
 function Home() {
+    const environment : string = import.meta.env.VITE_ENV;
 
     return (
         <div>
+            <p>App is running in {environment} environment</p>
             <p>Home page...</p>
         </div>
     );
