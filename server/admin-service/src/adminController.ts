1bbd2186d49c0507e670787cd3ae8a4f1e46133a
Ashwath Kumaran <a4ashwath@gmail.com>
2026-01-22 15:08:12 +0530

    started the microservice and laid the basic template

---
+++ server/admin-service/src/adminController.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, RequestHandler, Response } from 'express';
+
+const getUser: RequestHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    // if (!req.user) {
+    //     // next(new ApiError(403));
+    //     return;
+    // }
+    try {
+        const data = {
+            "Admin":"Ashwath", 
+            "info": req.user
+        };
+        res.json(data);
+    } catch (error: any) {
+        next();
+    }
+    return;
+};
+
+
+export default {
+    getUser
+};
